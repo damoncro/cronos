9ca7be18a0fab8dd05dbdfe87a812815fb55e288
Damon Chen <damon.chen@crypto.com>
2021-11-17 17:55:10 +0800

    Fetch and build ibc-rs in hermes.nix instead

--- nix/default.nix
+++ nix/default.nix
@@ -40,25 +40,6 @@ import sources.nixpkgs {
       };
     })
     (_: pkgs: { test-env = import ./testenv.nix { inherit pkgs; }; })
-    (_: pkgs: {
-      hermes = pkgs.rustPlatform.buildRustPackage rec {
-        name = "hermes";
-        src = sources.ibc-rs;
-        cargoSha256 = sha256:1sc4m4bshnjv021ic82c3m36pakf15xr5cw0dsrcjzs8pv3nq9cd;
-        cargoBuildFlags = "-p ibc-relayer-cli";
-        buildInputs = pkgs.lib.optionals pkgs.stdenv.isDarwin [
-          pkgs.darwin.apple_sdk.frameworks.Security
-          pkgs.darwin.libiconv
-        ];
-        doCheck = false;
-        RUSTFLAGS = "--cfg ossl111 --cfg ossl110 --cfg ossl101";
-        OPENSSL_NO_VENDOR = "1";
-        OPENSSL_DIR = pkgs.symlinkJoin {
-          name = "openssl";
-          paths = with pkgs.openssl; [ out dev ];
-        };
-      };
-    })
   ];
   config = { };
   inherit system;
